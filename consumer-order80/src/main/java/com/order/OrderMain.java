d406ad48e5f109a9b17ddfbaf3ea49dcf1e16749
wangy <wany-jishu@ejiaofei.com>
2020-08-30 16:20:20 +0800

    eureka

--- consumer-order80/src/main/java/com/order/OrderMain.java
+++ consumer-order80/src/main/java/com/order/OrderMain.java
@@ -2,6 +2,7 @@ package com.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @className: OrderMain
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2020/7/24
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
 
     public static void main(String[] args) {
--- provider-payment8001/src/main/java/com/payment/controller/PaymentController.java
+++ provider-payment8001/src/main/java/com/payment/controller/PaymentController.java
@@ -5,9 +5,12 @@ import cloud.common.entity.Payment;
 import com.payment.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -24,6 +27,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult creat(@RequestBody Payment payment) {
@@ -43,5 +49,20 @@ public class PaymentController {
             return new CommonResult(444, "未查询到数据");
         }
         return new CommonResult<>(200, "查询成功:" + serverPort, payment);
+    }
+
+    @GetMapping(value = "payment/discovery")
+    public DiscoveryClient discovery() {
+        // 所有服务
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("**************** service" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info("****************" + instance.getServiceId() + "\t" + instance.getHost() + "\t"
+                    + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
